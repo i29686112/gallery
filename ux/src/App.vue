9281d331daed1989e0699e69f2ef0fab8024a102
Ian <i29686112@gmail.com>
2020-11-15 13:21:08 +0800

    加入jslint
    嘗試把html轉成vue中

--- ux/src/App.vue
+++ ux/src/App.vue
@@ -3,12 +3,20 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+      <router-link to="/gallery">gallery</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style>
+
+@font-face {
+  font-family: "Merienda";
+  src: local("Merienda"),
+  url(./fonts/Font-Awesome-4.7/fonts/fontawesome-webfont.ttf) format("truetype");
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -29,4 +37,5 @@
 #nav a.router-link-exact-active {
   color: #42b983;
 }
+
 </style>
