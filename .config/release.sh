697c41151634d6a1220301d6c5a65d7b7e5580c9
Ian <i29686112@gmail.com>
2020-12-02 16:14:41 +0800

    Add heroku setup

---
+++ .config/release.sh
@@ -0,0 +1,4 @@
+php artisan storage:link
+mkdir -p ./storage/app/public/covers
+mkdir -p ./storage/app/public/photos
+cp -R ./.config/covers ./storage/app/public/covers
--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
+release: chmod u+x release.sh && ./.config/release.sh
 web: vendor/bin/heroku-php-apache2 public/
