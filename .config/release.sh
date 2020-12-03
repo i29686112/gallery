db9d509a6698b2765550c7abaf314a5299c84cce
Ian <i29686112@gmail.com>
2020-12-03 10:09:31 +0800

    Handle Heroku

--- .config/release.sh
+++ .config/release.sh
@@ -1,6 +1,8 @@
 php -v
 composer dump-autoload
 php artisan storage:link
-mkdir -p ../storage/app/public/photos
-mkdir -p ../storage/app/public/covers
-cp -R ./.config/covers ../storage/app/public/covers
+mkdir -p ./storage/app/public/covers
+mkdir -p ./storage/app/public/photos
+cp -R ./.config/covers ./storage/app/public/covers
+ls ./.config/covers -al
+ls ./storage/app/public/covers -al
